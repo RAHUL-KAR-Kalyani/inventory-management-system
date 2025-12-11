4d4e7ca536a576f9a75a78c84c1c8667931419db
Rahul Kar <108023178+RAHUL-KAR-Kalyani@users.noreply.github.com>
2025-12-11 14:37:19 +0530

    Remove authentication from dashboard route
    Removed authentication middleware from dashboard route.

--- server/routes/dashboardRouter.js
+++ server/routes/dashboardRouter.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const { getDashboardData } = require("../controllers/dashboardController.js");
-const isAuth = require("../middleware/isAuth.js");
+//const isAuth = require("../middleware/isAuth.js");
 
 
 const dashboardRouter = express.Router();
 
-dashboardRouter.get("/dashboard", isAuth, getDashboardData);
+dashboardRouter.get("/dashboard", getDashboardData);
 
 module.exports = dashboardRouter;
